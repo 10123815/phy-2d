dffd17ef874cf8b0700115616ed2b1f6543f42ea
10123815 <ysdysdysdysd123@163.com>
2016-07-23 15:29:50 +0800

    quad-tree remove

--- scene/quad-tree.cc
+++ scene/quad-tree.cc
@@ -10,7 +10,7 @@ void QuadTree::Insert(uint16_t id, const BaseCollider& collider)
 void QuadTree::InsertNode(uint16_t id, uint8_t deep, const BaseCollider& collider, std::shared_ptr<TreeNode> root)
 {
 	// The collider is in the rectangle bound.
-	if (BoundinBound(root->bound, collider.bound()) >= 0)
+	if (BoundinBound(root->bound, collider.bound()))
 	{
 		// If reach the max deep
 		if (deep >= max_deep_)
@@ -21,57 +21,62 @@ void QuadTree::InsertNode(uint16_t id, uint8_t deep, const BaseCollider& collide
 		}
 
 		// If the axis go across the bound.
-		Bound& root_bound = root->bound;
+		const Bound& root_bound = root->bound;
 		Vector2 center = (root_bound.max + root_bound.min) / 2;
-		if (AxisCrossBound(collider.bound(), center.x(), false) ||
-		        AxisCrossBound(collider.bound(), center.y()))
+		if (VertivalAxisCrossBound(collider.bound(), center.x()) ||
+			HorizentalAxisCrossBound(collider.bound(), center.y()))
 		{
 			// The collider is belong to this root area.
 			root->colliders.push_back(std::shared_ptr<BaseCollider>(&collider));
 			return;
 		}
 
+		// Insert the node in one of four child nodes.
 		for (uint8_t i = 0; i < 4; ++i)
 		{
-			if (root->children[i] != nullptr)
+			std::shared_ptr<TreeNode>* const children = root->children;
+			if (children[i] != nullptr)
 			{
-				if (BoundinBound(root->children[i]->bound, collider.bound()))
+				if (BoundinBound(children[i]->bound, collider.bound()))
 				{
 					// The collider is in child's bound.
-					InsertNode(id, deep + 1, collider, root->children[i]);
+					InsertNode(id, deep + 1, collider, children[i]);
 					return;
 				}
 				else
+				{
 					// The collider is out of the bound.
 					continue;
+				}
 			}
 			else
 			{
-				Bound bound = CreateBound(root.get(), i);
+				Bound bound = this->CreateBound(root.get(), i);
 				if (BoundinBound(bound, collider.bound()))
 				{
 					// The collider is in the bound.
 					// The child is null, create it first.
-					CreateQuadrant(root, i, bound);
-					InsertNode(id, deep + 1, collider, root->children[i]);
+					CreateChild(root, i, bound);
+					InsertNode(id, deep + 1, collider, children[i]);
 					return;
 				}
 				else
+				{
 					// The collider is out of the bound.
 					continue;
-
+				}
 			}
 		}
 	}
 }
 
-const Bound QuadTree::CreateBound(const TreeNode* root, uint8_t qr) const
+inline const Bound QuadTree::CreateBound(const TreeNode* root, uint8_t qr) const
 {
 	assert(qr <= 3);
 
 	// If collider is in the first quadrant, need to do nothing.
-	static const int8_t opx[] = {0, 1, 1, 0};
-	static const int8_t opy[] = {0, 0, 1, 1};
+	static const int8_t opx[] = { 0, 1, 1, 0 };
+	static const int8_t opy[] = { 0, 0, 1, 1 };
 
 	Bound bound;
 	// The first quadrant.
@@ -87,4 +92,54 @@ const Bound QuadTree::CreateBound(const TreeNode* root, uint8_t qr) const
 	bound.max += down * opy[qr];
 
 	return bound;
+}
+
+inline void ysd_phy_2d::QuadTree::CreateChild(std::shared_ptr<TreeNode> root, uint8_t qr, const Bound & bound)
+{
+	(root->children)[qr] = std::make_shared<TreeNode>();
+	(root->children)[qr]->bound = bound;
+}
+
+void QuadTree::Remove(uint16_t id, const Bound& bound)
+{
+	this->RemoveNode(id, bound, root_);
+}
+
+// Find the node recursively with given id and remove id.
+bool QuadTree::RemoveNode(uint16_t id, const Bound& bound, std::shared_ptr<TreeNode> root)
+{
+	if (root == nullptr)
+		return false;
+
+	// Find the collider in this tree node.
+	std::vector<std::shared_ptr<BaseCollider>>& colliders = root->colliders;
+	for (std::size_t i = 0, l = colliders.size(); i < l; i++)
+	{
+		if (colliders[i]->id() == id)
+		{
+			// We found it! Remove it.
+			colliders[i].reset();
+			colliders.erase(colliders.cbegin() + i);
+			return true;
+		}
+	}
+
+	// Find the collider in one of the four children.
+	for (std::size_t i = 0; i < 4; i++)
+	{
+		const std::shared_ptr<TreeNode> child = root->children[i];
+		if (child != nullptr)
+		{
+			const Bound& big_bound = root->children[i]->bound;
+			if (BoundinBound(big_bound, bound))
+			{
+				// It is here!! Find it in this child.
+				return this->RemoveNode(id, bound, root->children[i]);
+			}
+		}
+
+	}
+
+	// The node with the given id is not found.
+	return false;
 }
--- scene/quad-tree.h
+++ scene/quad-tree.h
@@ -30,43 +30,44 @@ struct TreeNode
 	// Rectangle bound.
 	Bound bound;
 
+	// 4 children.
+	std::shared_ptr<TreeNode> children[4];
+
 	// Colliders in this node.
 	std::vector<std::shared_ptr<BaseCollider>> colliders;
 
-	// 4 children.
-	std::shared_ptr<TreeNode> children[4];
 };
 
-class QuadTree
+class QuadTree final
 {
 public:
 
 	QuadTree(uint8_t max_deep)
-		: max_deep_(max_deep)
+		:root_(new TreeNode), max_deep_(max_deep)
 	{}
 
 	// Insert a new collider in the tree.
-	// @param[in] 	collider 	The collider need to insert in.
 	// @param[in] 	id  		The collider's id.
+	// @param[in] 	collider 	The collider need to insert in.
 	void Insert(uint16_t id, const BaseCollider& collider);
 
-	void Remove(uint16_t id);
-
-	void Search();
+	// Remove a collider from the tree with the given id and its AABB bound.
+	// @param[in]	id		The collider's id.
+	// @param[in]	bound	The collider's AABB bound.
+	void Remove(uint16_t id, const Bound& bound);
 
 private:
 	void InsertNode(uint16_t id, uint8_t deep, const BaseCollider& collider, std::shared_ptr<TreeNode> root);
+
+	// If remove the node successful, return true.
+	bool RemoveNode(uint16_t id, const Bound& bound, std::shared_ptr<TreeNode> root);
 
 	// Create bound for four quadrants.
 	// @param[in]	qr 	[0, 3].
 	inline const Bound CreateBound(const TreeNode* root, uint8_t qr) const;
 
 	// Create 1/4 rect in root's bound.
-	inline void CreateQuadrant(std::shared_ptr<TreeNode> root, uint8_t qr, const Bound& bound)
-	{
-		(root->children)[qr] = std::make_shared<TreeNode>();
-		root->children[qr]->bound = bound;
-	}
+	inline void CreateChild(std::shared_ptr<TreeNode> root, uint8_t qr, const Bound& bound);
 
 	std::shared_ptr<TreeNode> root_;
 
